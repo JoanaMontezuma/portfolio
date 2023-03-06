534deb88a4b2b21a8d8d84a43f33726578a7fae0
Joana Montezuma <119960830+JoanaMontezuma@users.noreply.github.com>
2023-03-06 14:32:36 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//menu-icon
+
+let menu =document.querySelector('.menu-icon');
+let navbar =document.querySelector('.navbar');
+
+menu.onclick =() =>{
+   navbar.classList.toggle ("open-menu")
+   menu.classList.toggle ("move")
+};
+
+// Quando se faz scroll se o hamburguer estiver aberto ele fecha automaticamente
+window.onscroll = () =>{
+    navbar.classList.remove ("open-menu")
+    menu.classList.remove ("move") 
+}
+
+// header background change on scroll
+
+let header = document.querySelector('header')
+window.addEventListener('scroll', () => {
+   header.classList.toggle ('header-active', window.scrollY > 0)
+});
+
+// scroll top
+
+let scrollTop = document.querySelector('.scroll-top')
+window.addEventListener('scroll', () => {
+   scrollTop.classList.toggle ('scroll-active', window.scrollY >= 400)
+});
+
+
+
+
